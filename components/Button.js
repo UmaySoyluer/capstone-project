ca4c8e8b2e713fa8c3153fdd49b1cf7373cbfb4d
Aleksandra <abaibik@gmail.com>
2023-10-12 14:08:47 +0200

    fix button to link

--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { HiArrowLeft, HiOutlineTrash } from "react-icons/hi2";
 
-export default function Button() {
+export default function BackLink() {
   return (
     <Link href={"/"}>
       <HiArrowLeft title="back" />
