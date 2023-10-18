be7a6ec159609c208f82a6640db481032857ccbf
Aleksandra <abaibik@gmail.com>
2023-10-18 15:58:39 +0200

    replace PUT to [taskId]

--- pages/api/projects/[id]/tasks/[taskId].js
+++ pages/api/projects/[id]/tasks/[taskId].js
@@ -16,5 +16,17 @@ export default async function handler(request, response) {
       return response.status(404).json({ status: "Task not found" });
     }
   }
+
+  if (request.method === "PUT") {
+    try {
+      const taskData = request.body;
+      await Task.findByIdAndUpdate(taskId, taskData);
+
+      return response.status(201).json({ message: "Task updated" });
+    } catch (error) {
+      console.error(`Can not edit task with id ${taskId}: ${error}`);
+      return response.status(400).json({ message: error.message });
+    }
+  }
   return response.status(405).json({ message: "Method not allowed." });
 }
