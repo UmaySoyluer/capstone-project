272ddbf1aba6b18c8476b1f06af97ac0ed487cae
Aleksandra <abaibik@gmail.com>
2023-10-19 14:58:37 +0200

    fix - no edit icon on task

--- pages/projects/[id]/tasks/[taskId]/index.js
+++ pages/projects/[id]/tasks/[taskId]/index.js
@@ -4,13 +4,9 @@ import styled from "styled-components";
 
 import Error from "@/components/Error";
 import Heading from "@/components/Heading";
-import { BackLink } from "@/components/Buttons";
+import { BackLink, EditLink } from "@/components/Buttons";
 import { DeleteButton } from "@/components/Buttons";
-
-const StyledContainer = styled.div`
-  padding-inline: 1rem;
-  margin-top: 1.5rem;
-`;
+import { StyledButtonContainer, StyledToolBar } from "../..";
 
 const StyledSection = styled.section`
   margin-top: 1rem;
@@ -50,10 +46,13 @@ export default function TaskDetailPage() {
   }
   return (
     <>
-      <StyledContainer>
+      <StyledButtonContainer>
         <BackLink href={`/projects/${id}`} />
-        <DeleteButton handleClick={handleDelete} />
-      </StyledContainer>
+        <StyledToolBar>
+          <DeleteButton handleClick={handleDelete} />
+          <EditLink url={`/projects/${id}/tasks/${taskId}/editTask`} />
+        </StyledToolBar>
+      </StyledButtonContainer>
       <Heading>Task : {title}</Heading>
       <StyledArticle>
         <h4>Description</h4>
