435c960cde98510722e254c68aea843ad58169e1
Aleksandra <abaibik@gmail.com>
2023-10-20 10:41:28 +0200

    install msw

---
+++ tests/index.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import ProjectsOverviewPage from "@/pages/index";
+
+describe("Home page", () => {
+  beforeEach(() => {
+    render(<ProjectsOverviewPage />);
+  });
+
+  it("renders a heading", () => {
+    const heading = screen.getByRole("heading", {
+      name: /project overview/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("renders a list of projects", async () => {
+    const projectList = await screen.findAllByRole("listitem");
+
+    expect(projectList).toBeGreaterThan(0);
+  });
+});
