c80e687a8241555c3bcb1b81b44a3fde8bf216e0
Umay Soyluer <umaysoyluer@gmail.com>
2023-10-20 15:34:55 +0200

    added localstorage and preference to the theme

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import ProjectList from "@/components/ProjectList";
 import styled from "styled-components";
 import { StyledToggleButton } from "@/components/Buttons";
 import { useTheme } from "next-themes";
+import { useEffect } from "react";
 
 const StyledMainHeadline = styled.h1`
   margin-block: 1rem;
@@ -13,12 +14,31 @@ const StyledMainHeadline = styled.h1`
 export default function ProjectsOverviewPage() {
   const { theme, setTheme } = useTheme();
 
+  function toggleTheme() {
+    const updatedTheme = theme === "dark" ? "light" : "dark";
+    setTheme(updatedTheme);
+    localStorage.setItem("theme", updatedTheme);
+  }
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    const prefersDark =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (savedTheme && ["dark", "light"].includes(savedTheme)) {
+      setTheme(savedTheme);
+    } else if (prefersDark) {
+      setTheme("dark");
+    }
+  }, []);
+
   return (
     <>
       <StyledToggleButton
         type="checkbox"
         id="toggle"
-        onChange={() => setTheme(theme === "light" ? "dark" : "light")}
+        onChange={toggleTheme}
       ></StyledToggleButton>
       <StyledMainHeadline>Project Overview</StyledMainHeadline>
       <ProjectList />
