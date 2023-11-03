7b1c6c9a3636a9771a9b8df10e8688da4ec90c16
Aleksandra <abaibik@gmail.com>
2023-11-03 11:21:58 +0100

    add not working tests

---
+++ __tests__/ProjectDetailPage.test.js
@@ -0,0 +1,50 @@
+import { screen, waitForElementToBeRemoved } from "@testing-library/react";
+import { customRender } from "@/testing-library-utils";
+import { useSession } from "next-auth/react";
+import ProjectDetailPage from "@/pages/projects/[id]";
+import mockRouter from "next-router-mock";
+import { createDynamicRouteParser } from "next-router-mock/dynamic-routes";
+import * as getWidth from "@/hooks/useWindowDimensions";
+
+describe("ProjectDetailPage when signed in", () => {
+  beforeEach(async () => {
+    useSession.mockReturnValue({
+      data: {
+        user: {
+          email: "foo@bar.com",
+        },
+      },
+    });
+
+    mockRouter.useParser(createDynamicRouteParser(["/projects/[id]"]));
+
+    customRender(<ProjectDetailPage />);
+
+    await waitForElementToBeRemoved(() => screen.getByTestId("spinnerId"));
+  });
+
+  it("renders project info", () => {
+    // jest
+    //   .spyOn(getWidth, "useDimensions")
+    //   .mockImplementation(() => [600, jest.fn()]);
+
+    mockRouter.push("/projects/1");
+
+    const projectTitle = screen.getByRole("heading", {
+      name: /super project/i,
+    });
+  });
+});
+
+describe("ProjectDetailPage page when signed out", () => {
+  beforeEach(async () => {
+    useSession.mockReturnValueOnce({ status: "unauthenticated" });
+    customRender(<ProjectDetailPage />);
+
+    await waitForElementToBeRemoved(() => screen.getByTestId("spinnerId"));
+  });
+
+  test("redirect to login page", () => {
+    expect(mockRouter.asPath).toEqual("/SignIn");
+  });
+});
